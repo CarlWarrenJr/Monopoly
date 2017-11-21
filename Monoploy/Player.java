2d5984bdc315e3a022c4d9007f91cc154a5092ec
CarlWarrenJr <33170958+CarlWarrenJr@users.noreply.github.com>
2017-11-21 14:52:56 -0700

    Add files via upload

--- Monoploy/Player.java
+++ Monoploy/Player.java
@@ -5,5 +5,6 @@ import java.util.ArrayList;
 public class Player {
 	public String Token;
 	public String name;
+	public final int INITIAL_MONEY=1500;
 	public ArrayList<String> possesions= new ArrayList<>();
 }

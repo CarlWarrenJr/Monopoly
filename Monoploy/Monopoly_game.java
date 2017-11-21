e3d76653c3cc4ee620d653f7684750f77c239c37
CarlWarrenJr <33170958+CarlWarrenJr@users.noreply.github.com>
2017-11-21 12:43:16 -0700

    Update Monopoly_game.java

--- Monoploy/Monopoly_game.java
+++ Monoploy/Monopoly_game.java
@@ -1,5 +1,18 @@
 package Monoploy;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Monopoly_game {
-
+  public static void main(String[] args) throws IOException{
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    do{
+    System.out.println("Are all players aged 8+?");
+    String ageResponse=in.readLine();
+    }while(!(ageResponse.equalsIgnoreCase("y")||ageResponse.equalsIgnoreCase("yes")||ageResponse.equalsIgnoreCase("n")||ageResponse.equalsIgnoreCase("no")))
+   if(ageResponse.equalsIgnoreCase("y")||ageResponse.equalsIgnoreCase("yes")){
+   //play game  
+  }
+    else{
+      //terminate since in the rules it says 8+
+    }
 }

d7919a3e8dc0663562fdf718aa9a5f4fb5cdc64c
CarlWarrenJr <33170958+CarlWarrenJr@users.noreply.github.com>
2017-11-21 18:07:31 -0700

    Add files via upload

--- Monoploy/Banker.java
+++ Monoploy/Banker.java
@@ -1,8 +1,39 @@
 package Monoploy;
-
+import java.util.ArrayList;
 public class Banker {
 	public final int hotels = 12;
 	public final int house = 32;
 	public int bal;
+	public ArrayList<String> properties=new ArrayList<>();
+	public void propertiesToBanker() {
+		properties.add("Atlantic Avenue");
+		properties.add("B & O Railroad");
+		properties.add("Baltic Avenue");
+		properties.add("Boardwalk");
+		properties.add("Connecticut Avenue");
+		properties.add("Electric Company");
+		properties.add("Illinois Avenue");
+		properties.add("Indiana Avenue");
+		properties.add("Kentucky Avenue");
+		properties.add("Marvin Gardens");
+		properties.add("Mediterranean Avenue");
+		properties.add("New York Avenue");
+		properties.add("North Carolina Avenue");
+		properties.add("Oriental Avenue");
+		properties.add("Pacific Avenue");
+		properties.add("Park Place");
+		properties.add("Pennsylvania Avenue");
+		properties.add("Pennsylvania Railroad");
+		properties.add("Reading Railroad");
+		properties.add("Short Line");
+		properties.add("St. Charles Place");
+		properties.add("St. James Place");
+		properties.add("States Avenue");
+		properties.add("Tennessee Avenue");
+		properties.add("Ventnor Avenue");
+		properties.add("Vermont Avenue");
+		properties.add("Virginia Avenue");
+		properties.add("Water Works");
+	}
 	
 }

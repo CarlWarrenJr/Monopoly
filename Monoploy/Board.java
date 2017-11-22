85107c83d5228404195dbb4b443fcb30a553bbc3
CarlWarrenJr <33170958+CarlWarrenJr@users.noreply.github.com>
2017-11-22 11:39:24 -0700

    Add files via upload

--- Monoploy/Board.java
+++ Monoploy/Board.java
@@ -1,46 +1,56 @@
 package Monoploy;
 
 public class Board {
-
- public void boardSetUp() {
+	public Banker banker=new Banker();
+	 public void boardSetUp() {
+		 setUpPlayers();
+	 }
 	 
- }
- 
- 
- public void boardPrint() {
-	 System.out.println(" __________________________________________________________________________________");
-	 System.out.println("|          |                                                          |           |");
-	 System.out.println("|		    |														   | 	       |");
-	 System.out.println("|		    |														   |Go To      |");
-	 System.out.println("|		    |														   | Jail      |");
-	 System.out.println("|__________|__________________________________________________________|___________|");
-	 System.out.println("|		    |														   |  	       |");
-	 System.out.println("|		    |														   |   	       |");
-	 System.out.println("|		    |														   |   	       |");
-	 System.out.println("|		    |		____________									   |  	       |");
-	 System.out.println("|		    |		|  Chance  |									   |  	       |");
-	 System.out.println("|		    |		|	Card   | 									   |  	       |");
-	 System.out.println("|		    |		|__________|			← ╫						   |  	       |");
-	 System.out.println("|		    |														   |  	       |");
-	 System.out.println("|		    |														   |  	       |");
-	 System.out.println("|		    |														   |  	       |");
-	 System.out.println("|		    |						MONOPOLY						   |  	       |");
-	 System.out.println("|		    |			The Fast Dealing Property Trading Game  	   |  	       |");
-	 System.out.println("|		    |														   |  	       |");
-	 System.out.println("|		    |														   |  	       |");
-	 System.out.println("|		    |									   ______________	   |   	       |");
-	 System.out.println("|		    |									   |  Community |	   |  	       |");
-	 System.out.println("|		    |								       |   Chest    |	   |  	       |");
-	 System.out.println("|		    |									   |____________|	   |  	       |");
-	 System.out.println("|		    |														   |  	       |");
-	 System.out.println("|		    |														   |  	       |");
-	 System.out.println("|__________|__________________________________________________________|___________|");
-	 System.out.println("|   |  IN  |          |          |        |    |     |     |     |    |           |");
-	 System.out.println("| J |╫╫╫╫╫ |		   |		  | 			|     | 	|	  |    |   ←       |");
-	 System.out.println("| U |╫╫╫╫╫ |		   |		  |	    		|     | 	|	  |    |   GO      |");
-	 System.out.println("| S | Jail |	  	   |	      |				|     | 	|	  |    |  	       |");
-	 System.out.println("| T |______|		   |	      |				|     | 	|	  |    |  	       |");
-	 System.out.println("| Visting  |		   |		  | 			|     | 	|	  |    |  	       |");
-	 System.out.println("|__________|__________|__________|_____________|_____|_____|_____|____|___________|");
- }
-}
+	 
+	 private void setUpPlayers() {
+		 //ask how many players and ask for names
+		for(int i=0;/*i<numPlayers*/;i++) {
+			//player instance.money.add(banker.bill500).... for all the bills
+		}
+		
+	}
+
+
+	public void boardPrint() {
+		 System.out.println(" __________________________________________________________________________________");
+		 System.out.println("|          |                                                          |           |");
+		 System.out.println("|		    |														   | 	       |");
+		 System.out.println("|		    |														   |Go To      |");
+		 System.out.println("|		    |														   | Jail      |");
+		 System.out.println("|__________|__________________________________________________________|___________|");
+		 System.out.println("|		    |														   |  	       |");
+		 System.out.println("|		    |														   |   	       |");
+		 System.out.println("|		    |														   |   	       |");
+		 System.out.println("|		    |		____________									   |  	       |");
+		 System.out.println("|		    |		|  Chance  |									   |  	       |");
+		 System.out.println("|		    |		|	Card   | 									   |  	       |");
+		 System.out.println("|		    |		|__________|			← ╫						   |  	       |");
+		 System.out.println("|		    |														   |  	       |");
+		 System.out.println("|		    |														   |  	       |");
+		 System.out.println("|		    |														   |  	       |");
+		 System.out.println("|		    |						MONOPOLY						   |  	       |");
+		 System.out.println("|		    |			The Fast Dealing Property Trading Game  	   |  	       |");
+		 System.out.println("|		    |														   |  	       |");
+		 System.out.println("|		    |														   |  	       |");
+		 System.out.println("|		    |									   ______________	   |   	       |");
+		 System.out.println("|		    |									   |  Community |	   |  	       |");
+		 System.out.println("|		    |								       |   Chest    |	   |  	       |");
+		 System.out.println("|		    |									   |____________|	   |  	       |");
+		 System.out.println("|		    |														   |  	       |");
+		 System.out.println("|		    |														   |  	       |");
+		 System.out.println("|__________|__________________________________________________________|___________|");
+		 System.out.println("|   |  IN  |          |          |        |    |     |     |     |    |           |");
+		 System.out.println("| J |╫╫╫╫╫ |		   |		  | 			|     | 	|	  |    |   ←       |");
+		 System.out.println("| U |╫╫╫╫╫ |		   |		  |	    		|     | 	|	  |    |   GO      |");
+		 System.out.println("| S | Jail |	  	   |	      |				|     | 	|	  |    |  	       |");
+		 System.out.println("| T |______|		   |	      |				|     | 	|	  |    |  	       |");
+		 System.out.println("| Visting  |		   |		  | 			|     | 	|	  |    |  	       |");
+	System.out.println("|__________|__________|__________|_____________|_____|_____|_____|____|___________|");
+	 }
+	}
+
